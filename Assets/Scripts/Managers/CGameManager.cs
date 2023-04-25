4996e10c84ec9ec9f6cba072e7897107094020c4
phatlam1811 <phatlam1811@gmail.com>
2023-04-25 23:21:48 +0700

    add CGameManager, CGamePlayManager, change sprites resolution

---
+++ Assets/Scripts/Managers/CGameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CGameManager : MonoSingleton<CGameManager>
+{
+    private void Start()
+    {
+        this.StartGame();
+    }
+
+    void StartGame()
+    {
+        // Load Game Data
+        // Load Game Configs
+        // Start Gameplay
+    }
+}
---
+++ Assets/Scripts/Managers/CGamePlayManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CGamePlayManager : MonoBehaviour
+{
+    private void Start()
+    {
+                
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void GameStart()
+    {
+
+    }
+}
